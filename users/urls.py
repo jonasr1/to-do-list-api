c516c8cb67f50e9743a8af31c916bc09d2155baf
jonasr1 <yatosama924@gmail.com>
2025-01-09 14:31:08 -0300

    refactor(users): add 'token/verify/'route and improve authetication in UserView

--- users/urls.py
+++ users/urls.py
@@ -3,11 +3,13 @@ from users.views import RegisterView, UserView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('user/', UserView.as_view()),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('me/', UserView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,13 +1,9 @@
 from datetime import datetime, timezone
-from typing import Any, Dict
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.exceptions import AuthenticationFailed
-from core import settings
-from users.models import User
+from rest_framework.permissions import IsAuthenticated
 from users.serializers import UserSerializer
-import jwt
 
 class RegisterView(APIView):
     def post(self, request: Request) -> Response:
@@ -18,15 +14,9 @@ class RegisterView(APIView):
     
 
 class UserView(APIView):
+    permission_classes = [IsAuthenticated] # Garante que a requisição precise de autenticação
     def get(self, request: Request) -> Response:
-        auth_header = request.headers.get('Authorization')
-        if not auth_header or not auth_header.startswith('Bearer'):
-            raise AuthenticationFailed('Token not provided!')
-        token = auth_header.split(' ')[1]
-        payload = self.decode_token(token)
-        user = User.objects.filter(id=payload['user_id']).first()
-        if user is None:
-            raise AuthenticationFailed('User not found')
+        user = request.user  # O usuário é automaticamente extraído da requisição pelo SimpleJWT
         return Response({
             'user': {
                 'user_id': user.id,
@@ -34,24 +24,11 @@ class UserView(APIView):
                 'is_active': user.is_active,
             },
             'token': {
-                'token_type': payload['token_type'],
-                'exp': self.convert_timestamp(payload['exp']),
-                'iat': self.convert_timestamp(payload['iat'])
+                'token_type': request.auth['token_type'],
+                'exp': self.convert_timestamp(request.auth['exp']),
+                'iat': self.convert_timestamp(request.auth['iat'])
             }
         })
         
-    def decode_token(self, token: str) -> Dict[str, Any]:
-        try:
-            payload = jwt.decode(
-                jwt=token,
-                key=settings.SIMPLE_JWT['SIGNING_KEY'],
-                algorithms=settings.SIMPLE_JWT['ALGORITHM']
-            )
-            return payload
-        except jwt.ExpiredSignatureError:
-            raise AuthenticationFailed('Token expired')
-        except jwt.InvalidTokenError:
-            raise AuthenticationFailed('Invalid token')
-    
     def convert_timestamp(self, timestamp:float):
         return datetime.fromtimestamp(timestamp=timestamp, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
